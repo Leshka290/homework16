14b3121525995c510e01d2612f568356729c554b
leshka290 <lleesshhkkaa00@gmail.com>
2023-04-28 15:24:30 +0300

    Доработка ДЗ 2

--- src/main/java/my/project/calculator/CalculatorServiceImp.java
+++ src/main/java/my/project/calculator/CalculatorServiceImp.java
@@ -12,23 +12,23 @@ public class CalculatorServiceImp implements CalculatorService {
 
     @Override
     public String plus(int num1, int num2) {
-        return "num1 + num2 = " + (num1 + num2);
+        return num1 + " + " + num2 + " = " + (num1 + num2);
     }
 
     @Override
     public String minus(int num1, int num2) {
-        return "num1 - num2 = " + (num1 - num2);
+        return num1 + " - " + num2 + " = " + (num1 - num2);
     }
 
     @Override
     public String multiply(int num1, int num2) {
-        return "num1 * num2 = " + (num1 * num2);
+        return num1 + " * " + num2 + " = " + (num1 * num2);
     }
 
     @Override
     public String divide(int num1, int num2) {
         try {
-            return "num1 / num2 = " + (num1 / num2);
+            return num1 + " / " + num2  + " = " +  (num1 / num2);
         } catch (ArithmeticException e) {
             return e.toString();
         }
